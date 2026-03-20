8da4ff40f2862b76b13c8045ca44a499fcc8e0f3
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-20 23:07:49 +0530

    enough for the day

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useAuth } from '../features/auth/hook/useAuth'
+import { useEffect } from 'react'
 
 const App = () => {
+
+  const auth = useAuth()
+
+  useEffect(() => {
+    auth.handleGetme()
+  }, [])
   return (
+
     <div>App</div>
   )
 }
