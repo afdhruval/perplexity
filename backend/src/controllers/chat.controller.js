ca996acc6badf2f63faceae6bba133060cd08e05
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-03-22 18:57:03 +0530

    get chats, delete , get messages api done

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -46,3 +46,67 @@ export async function sendMessage(req, res) {
 }
 
 
+export async function getChats(req, res) {
+
+    const user = req.user
+
+    const chat = await chatModel.find({ user: user.id })
+
+    res.status(200).json({
+        message: "chatts recieved successfully",
+        chat
+    })
+
+}
+
+export async function getMessages(req, res) {
+
+    const { chatId } = req.params
+
+    const chat = await chatModel.findOne({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "chat not found"
+        })
+    }
+
+    const messages = await messageModel.find({
+        chat: chatId
+    })
+
+    res.status(200).json({
+        message: "message retrived successfully",
+        messages
+    })
+}
+
+export async function deleteChat(req, res) {
+
+    const { chatId } = req.params
+
+    const chat = await chatModel.findOneAndDelete({
+        _id: chatId,
+        user: req.user.id
+    })
+
+    await messageModel.deleteMany({
+        chat: chatId
+    })
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "chat not found"
+        })
+    }
+
+    res.status(200).json({
+        message: "chat deleted successfully"
+    })
+
+}
+
+
